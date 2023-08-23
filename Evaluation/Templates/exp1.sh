c7a836cc8679fe52e5949239ec25ea346eecd344
alejandrofontan <fontanvillacampa@gmail.com>
2023-08-23 15:45:37 +1000

    add sequences to experiment

--- Evaluation/Templates/exp1.sh
+++ Evaluation/Templates/exp1.sh
@@ -1,16 +1,19 @@
 
 exp="GBA_orb"
-numRuns=1
+numRuns=5
 #rm -rf "/home/alex/${exp}"
 
-./build_ORB_SLAM2_fast.sh ORB_SLAM2_Distribution /home/alex 
+#./build_ORB_SLAM2.sh ORB_SLAM2_Distribution /home/alex 
 
-./createResultsFolder.sh "/home/alex/${exp}"
+#./createResultsFolder.sh "/home/alex/${exp}"
 
 #./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} eth table_3 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
+./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} kitti 03 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
 #./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} kitti 04 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
-./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} rgbdtum rgbd_dataset_freiburg3_structure_texture_far 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
+#./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} rgbdtum rgbd_dataset_freiburg3_structure_texture_far 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
 #./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} rgbdtum rgbd_dataset_freiburg1_xyz 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
 #./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} kitti 06 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
 #./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} euroc V2_01_easy 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
 #./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} rgbdtum rgbd_dataset_freiburg2_desk 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
+#./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} monotum sequence_23 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
+#./run_SLAM.sh orbslam2 mono test1 2023_06_23 y 1 ${numRuns} nuim traj0_frei_png 0 /home/alex vocabulary ORB_SLAM2_Distribution ${exp}
